c36ced85da2fb288452df99c2d600679b6da84ed
Juliju <askri@ukr.net>
2020-07-14 21:22:57 +0300

    Список категорий вопросов

--- src/main/java/com/julia/bot/BotLogic.java
+++ src/main/java/com/julia/bot/BotLogic.java
@@ -2,11 +2,13 @@ package com.julia.bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +30,7 @@ public class BotLogic extends TelegramLongPollingBot {
                 SendMessage message = new SendMessage()     // определяет сообщение в чат
                         .setChatId(chatId)
                         .setText(TextMessage.TEXT_START);
-                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();//клавиатура
                 List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
 
                 for (NameList i : JsonRead.jsonRead(TextMessage.LANG_PATH)) {  //значения кнопок из json файла
@@ -50,16 +52,34 @@ public class BotLogic extends TelegramLongPollingBot {
         }
     }
 
-      private void workCallback(Update update) { //получение сообщения от пользователя
+    private void workCallback(Update update) { //получение сообщения от пользователя
         Message callbackMessage = update.getCallbackQuery().getMessage();
         String callbackData = update.getCallbackQuery().getData();
 
 
         String[] res = callbackData.split("-");
 
-        SendMessage message = new SendMessage()     // определяет сообщение в чат
+        switch (res[0]) {
+            case "language":
+                getLanguageCategories(callbackMessage, res);
+                break;
+            case "sections":
+                break;
+        }
+
+    }
+
+    private void getLanguageCategories(Message callbackMessage, String[] res) {
+
+        EditMessageText message = new EditMessageText()     // определяет сообщение в чат
                 .setChatId(callbackMessage.getChatId())
+                .setMessageId(callbackMessage.getMessageId())
                 .setText(TextMessage.TEXT_SECT);
+
+        EditMessageText messageUserCall = new EditMessageText()     // определяет сообщение в чат
+                .setChatId(callbackMessage.getChatId())
+                .setMessageId(callbackMessage.getMessageId())
+                .setText("\uD83D\uDCA4");
 
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
@@ -75,11 +95,16 @@ public class BotLogic extends TelegramLongPollingBot {
         // Добавляем в сообщение
         markupInline.setKeyboard(rowsInline);
         message.setReplyMarkup(markupInline);
-        try {
-            execute(message); // отправляем сообщение пользователю
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            try {
+                if (JsonRead.jsonRead(TextMessage.SECT_PATH).get(0).getCallback().contains(res[1])) {
+                    execute(message); // отправляем сообщение пользователю
+                } else {
+                    execute(messageUserCall);
+                }
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
         }
+
     }
 
     public String getBotUsername() {    //Возвращает имя бота
@@ -90,10 +115,8 @@ public class BotLogic extends TelegramLongPollingBot {
         return TextMessage.BOT_TOKEN;
     }
 
+
 }
-
-
-
 
 
 
